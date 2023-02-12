fa6a74a0947d2e5c8e4e4f98bc0f85cb36a2cb13
talktoacomputer <saiavinash.147@gmail.com>
2023-02-12 23:15:35 +0530

    cleanup home page

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Abroad Assist welcomes you!</title>
+        <meta name="description" content="Abroad Assist welcomes you!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-center">Home page of Abroad Assist</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
