23ccfe7897fc272522e7b8a2f9ba984db21d1b83
talktoacomputer <saiavinash.147@gmail.com>
2023-02-14 00:16:48 +0530

    add styles to footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -3,7 +3,11 @@ import React, { useMemo } from "react";
 const Footer = ({}) => {
   const year = useMemo(() => new Date().getFullYear(), []);
 
-  return <footer>Copyright © {year} Abroad Assist</footer>;
+  return (
+    <footer className="m-0 py-4 mx-auto flex flex-row justify-center bg-aa-inner text-slate-100 font-sans font-thin">
+      <span className="">Copyright © {year} Abroad Assist</span>
+    </footer>
+  );
 };
 
 export default Footer;
