4c2656e71282144a9fd3bb649025a8b330a16aed
talktoacomputer <saiavinash.147@gmail.com>
2023-02-14 23:14:48 +0530

    update Header styles

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,3 +1,4 @@
+import Container from "components/wrappers/Container";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -25,34 +26,36 @@ const Header = ({}) => {
 
   return (
     <header className="m-0 shadow-lg">
-      <nav className="sm:container sm:mx-auto py-4 px-3">
-        <div className=" flex flex-row justify-between items-center">
-          <Link href="/">
-            <Image
-              alt="Abroad Assist ®"
-              src="/assets/images/registered-logo.png"
-              width={200}
-              height={30}
-            />
-          </Link>
-          <div className="flex flex-row justify-evenly">
-            {pages.map((page, index) => {
-              const isCurrentPage = router?.pathname === page.path;
-              return (
-                <Link href={page.path}>
-                  <span
-                    key={"nav-link-" + index}
-                    className={`m-1 py-3 px-6 capitalize text-lg font-semibold hover:bg-slate-200 rounded-md transition-colors duration-300 ease-in-out shadow-none ${
-                      isCurrentPage ? "bg-slate-300 text-aa-outer shadow" : ""
-                    }`}
-                  >
-                    {page.label}
-                  </span>
-                </Link>
-              );
-            })}
+      <nav className="py-4 px-3">
+        <Container>
+          <div className=" flex flex-col md:flex-row justify-between items-center">
+            <Link href="/">
+              <Image
+                alt="Abroad Assist ®"
+                src="/assets/images/registered-logo.png"
+                width={200}
+                height={30}
+              />
+            </Link>
+            <div className="my-4 md:my-0 flex flex-col sm:flex-row justify-evenly">
+              {pages.map((page, index) => {
+                const isCurrentPage = router?.pathname === page.path;
+                return (
+                  <Link href={page.path} className="sm:m-1 my-3 mx-auto">
+                    <span
+                      key={"nav-link-" + index}
+                      className={`py-3 px-6 capitalize text-lg font-semibold hover:bg-slate-200 rounded-md transition-colors duration-300 ease-in-out shadow-none ${
+                        isCurrentPage ? "bg-slate-300 text-aa-outer shadow" : ""
+                      }`}
+                    >
+                      {page.label}
+                    </span>
+                  </Link>
+                );
+              })}
+            </div>
           </div>
-        </div>
+        </Container>
       </nav>
     </header>
   );
