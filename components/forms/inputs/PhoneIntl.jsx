6e2c9d9c434209b11b3538c8395142a05f983278
talktoacomputer <saiavinash.147@gmail.com>
2023-07-20 11:03:34 +0530

    add default values to Phone

--- components/forms/inputs/PhoneIntl.jsx
+++ components/forms/inputs/PhoneIntl.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 
-const PhoneIntl = ({ value, onBlur, onChange }) => {
+const PhoneIntl = ({ value = "+91", onBlur = null, onChange = null }) => {
   return (
     <PhoneInput
       {...{
         country: "us",
         value,
         onChange,
+        onBlur,
         inputClass: `mt-0 w-full rounded-md border-gray-200 shadow-sm sm:text-sm`,
       }}
     />
