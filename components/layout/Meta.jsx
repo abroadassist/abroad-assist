37305cfc6b6a9611c54f9182882c4bcbd3a25e4d
talktoacomputer <saiavinash.147@gmail.com>
2023-07-31 11:19:06 +0530

    add meta image

--- components/layout/Meta.jsx
+++ components/layout/Meta.jsx
@@ -5,6 +5,7 @@ const Meta = ({ siteTitle = "Abroad Assist", siteDescription = "Abroad Assist we
     <Head>
       <title>{siteTitle}</title>
       <meta name="description" content={siteDescription} />
+      <meta property="og:image" content="/assets/images/MetaImage.png"></meta>
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
