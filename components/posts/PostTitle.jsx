7ebba35be80574b22a7e57426078991fa5af9311
talktoacomputer <saiavinash.147@gmail.com>
2023-08-11 11:42:25 +0530

    add working blog engine

---
+++ components/posts/PostTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostTitle = ({ title }) => {
+  return <h1 className="text-5xl font-heading  font-bold">{title}</h1>;
+};
+
+export default PostTitle;
---
+++ constants/values.js
@@ -0,0 +1,17 @@
+export const blogCategories = {
+  "tag 1": {
+    path: "tag-1",
+  },
+  "tag 2": {
+    path: "tag-2",
+  },
+  "tag 3": {
+    path: "tag-3",
+  },
+  "tag 4": {
+    path: "tag-444",
+  },
+  "tag 5": {
+    path: "tag-5",
+  },
+};
