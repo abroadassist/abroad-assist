6bccb4726e8a7d46d165070906f6e1db5de0e531
talktoacomputer <saiavinash.147@gmail.com>
2023-08-24 09:45:51 +0530

    update container for Post

--- components/wrappers/PostContainer.jsx
+++ components/wrappers/PostContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PostContainer = ({ children, classes }) => {
-  return <div className={`mx-auto max-w-2xl py-4  ${classes}`}>{children}</div>;
+  return <div className={`mx-auto max-w-2xl py-4 px-4 md:px-0  ${classes}`}>{children}</div>;
 };
 
 export default PostContainer;
