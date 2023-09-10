1f51d12172c3426760ebd9a26716cb98481b336d
talktoacomputer <saiavinash.147@gmail.com>
2023-09-10 17:52:09 +0530

    update styles for images and h1

--- components/posts/PostTitle.jsx
+++ components/posts/PostTitle.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const PostTitle = ({ title }) => {
-  return <h1 className=" text-3xl md:text-4xl lg:text-5xl mt-3 mb-8 font-semibold font-jumbo_heading">{title}</h1>;
+  return (
+    <h1
+      className=" text-3xl md:text-4xl lg:text-5xl mt-3 mb-8 font-semibold font-jumbo_heading"
+      style={{
+        lineHeight: "1.15 !important", // having to do inline styles as the classnames are getting overriden by tailwind styles despite putting them after the classes
+      }}
+    >
+      {title}
+    </h1>
+  );
 };
 
 export default PostTitle;
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["abroadassist-assets.s3.ap-south-1.amazonaws.com"],
+    domains: ["d1zehk3t3kkc0c.cloudfront.net"],
   },
 };
 
